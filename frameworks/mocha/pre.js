c0831fa287297813050bed73cd15ff4da8767524
Roman Shtylman <shtylman@gmail.com>
2013-12-17 22:06:08 -0500

    wip

--- frameworks/mocha/pre.js
+++ frameworks/mocha/pre.js
@@ -1,3 +1,6 @@
+var eio = require('engine.io-client');
+var win = window;
+
 // convert zuul mocha ui's to our ui
 var ui_map = {
   'mocha-bdd': 'bdd',
@@ -5,7 +8,64 @@ var ui_map = {
   'mocha-tdd': 'tdd'
 };
 
-// TODO(shtylman) setup mocha?
 mocha.setup({
   ui: ui_map[zuul.ui]
+});
+
+var harness = global.mochaPhantomJS || mocha;
+if (harness.checkLeaks) {
+  harness.checkLeaks();
+}
+
+var socket = eio({ path: '/__zuul/eio' });
+socket.onopen = function() {
+  console.log('opened');
+
+  socket.onmessage = function(data){
+    console.log('msg');
+  };
+
+  socket.onclose = function(){};
+};
+
+var runner = harness.run();
+
+var suite = harness.suite;
+if (suite.suites.length === 0 && suite.tests.length === 0) {
+  window.zuul_results = {
+    failures: 0,
+    passed: false
+  };
+}
+
+var failed = [];
+
+runner.on('pass', function(test) {
+});
+
+runner.on('fail', function(test, err) {
+  failed.push({
+    title: test.title,
+    fullTitle: test.fullTitle(),
+    error: {
+      message: err.message,
+      stack: err.stack
+    }
+  });
+});
+
+// so.. now the question is ...
+// what do we do for identifying which browser results we got back?
+
+runner.on('end', function() {
+  runner.stats.failed = failed;
+  runner.stats.passed = failed.length === 0;
+  win.zuul_results = runner.stats;
+
+  // so the only additional thing is we need to know for what browser
+  // this was for
+  socket.send(JSON.stringify({
+    type: 'done',
+    results: runner.stats
+  }));
 });
