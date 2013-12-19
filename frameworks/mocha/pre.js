1346db952b397da8428d458a4ac4d0714f936721
Roman Shtylman <shtylman@gmail.com>
2013-12-18 22:14:28 -0500

    wip

--- frameworks/mocha/pre.js
+++ frameworks/mocha/pre.js
@@ -1,6 +1,9 @@
 var eio = require('engine.io-client');
 var win = window;
 
+// assigned by zuul so it knows which browser these tests are for
+var browser_id = zuul.browser_id;
+
 // convert zuul mocha ui's to our ui
 var ui_map = {
   'mocha-bdd': 'bdd',
@@ -12,60 +15,68 @@ mocha.setup({
   ui: ui_map[zuul.ui]
 });
 
+var socket = eio({ path: '/__zuul/eio' });
+socket.onopen = function() {
+  console.log('opened');
+
+  // load test bundle
+  var script = document.createElement('script');
+  script.onload = run_tests;
+  script.async = true;
+  script.src = '/__zuul/test-bundle.js';
+  document.body.appendChild(script);
+
+  socket.onclose = function() {
+    console.log('closed');
+    // TODO??
+  };
+};
+
 var harness = global.mochaPhantomJS || mocha;
 if (harness.checkLeaks) {
   harness.checkLeaks();
 }
 
-var socket = eio({ path: '/__zuul/eio' });
-socket.onopen = function() {
-  console.log('opened');
+function run_tests() {
+  var suite = harness.suite;
+  if (suite.suites.length === 0 && suite.tests.length === 0) {
+    // no tests to run
+    // TODO report done
+    return;
+  }
 
-  socket.onmessage = function(data){
-    console.log('msg');
-  };
+  var runner = harness.run();
 
-  socket.onclose = function(){};
-};
+  var failed = [];
 
-var runner = harness.run();
+  runner.on('pass', function(test) {
+  });
 
-var suite = harness.suite;
-if (suite.suites.length === 0 && suite.tests.length === 0) {
-  window.zuul_results = {
-    failures: 0,
-    passed: false
-  };
+  runner.on('fail', function(test, err) {
+    failed.push({
+      title: test.title,
+      fullTitle: test.fullTitle(),
+      error: {
+        message: err.message,
+        stack: err.stack
+      }
+    });
+  });
+
+  runner.on('end', function() {
+    console.log('done');
+    runner.stats.failed = failed;
+    runner.stats.passed = failed.length === 0;
+    //win.zuul_results = runner.stats;
+
+    // so the only additional thing is we need to know for what browser
+    // this was for
+    socket.send(JSON.stringify({
+      type: 'done',
+      browser_id: browser_id,
+      results: runner.stats
+    }));
+  });
 }
 
-var failed = [];
 
-runner.on('pass', function(test) {
-});
-
-runner.on('fail', function(test, err) {
-  failed.push({
-    title: test.title,
-    fullTitle: test.fullTitle(),
-    error: {
-      message: err.message,
-      stack: err.stack
-    }
-  });
-});
-
-// so.. now the question is ...
-// what do we do for identifying which browser results we got back?
-
-runner.on('end', function() {
-  runner.stats.failed = failed;
-  runner.stats.passed = failed.length === 0;
-  win.zuul_results = runner.stats;
-
-  // so the only additional thing is we need to know for what browser
-  // this was for
-  socket.send(JSON.stringify({
-    type: 'done',
-    results: runner.stats
-  }));
-});
