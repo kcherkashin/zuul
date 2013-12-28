c88da965a7304c1e75f5632202d006b1ac36c35b
Roman Shtylman <shtylman@gmail.com>
2013-12-28 15:39:26 -0500

    wip

--- frameworks/mocha/pre.js
+++ frameworks/mocha/pre.js
@@ -1,3 +1,11 @@
+var http = require('http');
+var load_script = require('load-script');
+var JSON = global.JSON || require('JSON2');
+
+try {
+  var stack_mapper = require('stack-mapper');
+} catch (err) {};
+
 // convert zuul mocha ui's to our ui
 var ui_map = {
   'mocha-bdd': 'bdd',
@@ -9,3 +17,107 @@ var ui_map = {
 mocha.setup({
   ui: ui_map[zuul.ui]
 });
+
+var harness = window.mochaPhantomJS || mocha;
+if (harness.checkLeaks) {
+  harness.checkLeaks();
+}
+
+load_script('/__zuul/test-bundle.js', run);
+
+function run(err) {
+  if (err) {
+    window.zuul_results = {
+      failures: 0,
+      passed: false
+    };
+    return;
+  }
+
+  if (!stack_mapper) {
+    return do_run();
+  }
+
+  var map = undefined;
+  var mapper = undefined;
+
+  var opt = {
+    path: '/__zuul/test-bundle.map.json'
+  };
+
+  http.get(opt, function(res) {
+    var body = '';
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+
+    res.on('end', function() {
+      map = JSON.parse(body);
+      mapper = stack_mapper(map);
+      do_run();
+    });
+  });
+
+  function do_run() {
+
+    var runner = harness.run();
+
+    var suite = harness.suite;
+    if (suite.suites.length === 0 && suite.tests.length === 0) {
+      window.zuul_results = {
+        failures: 0,
+        passed: false
+      };
+    }
+
+    var failed = [];
+
+    var tracekit = require('tracekit');
+    tracekit.collectWindowErrors = false;
+
+    runner.on('fail', function(test, err) {
+
+      var report = tracekit.computeStackTrace(err);
+
+      var stack = report.name + ': ' + report.message;
+
+      var stacks = report.stack;
+      for (var i = 0; i <stacks.length; ++i) {
+        var item = stacks[i];
+
+        stack += '\n\tat ' + item.func + ' (' + item.url + ':' + item.line;
+
+        if (item.column) {
+          stack += ':' + item.column;
+        }
+        else {
+          stack += ':0';
+        }
+
+        stack += ')';
+      }
+
+      // firefox stack traces will cause this to fail
+      if (mapper && stack) {
+        var include_source = false;
+        var info = mapper.map(stack, include_source);
+        err.stack = info.stack;
+      }
+
+      failed.push({
+        title: test.title,
+        fullTitle: test.fullTitle(),
+        error: {
+          message: err.message,
+          stack: err.stack
+        }
+      });
+    });
+
+    runner.on('end', function(){
+      runner.stats.failed = failed;
+      runner.stats.passed = failed.length === 0;
+      window.zuul_results = runner.stats;
+    });
+  };
+}
--- frameworks/tape/pre.js
+++ frameworks/tape/pre.js
@@ -1,17 +1,13 @@
 var test = require('tape');
 var through = require('through');
+var parser = require('tap-parser');
+var inspect = require('util').inspect;
+
+var load_script = require('load-script');
+var stack_mapper = require('stack-mapper');
+var http = require('http');
 
 process.stdout = through();
-
-var finished = require('tap-finished');
-var stream = finished(function (results) {
-    var failed = results.fail;
-    window.zuul_results = {
-        failed: failed,
-        passed: failed.length === 0
-    };
-});
-process.stdout.pipe(stream);
 
 var params = (function () {
     var unesc = typeof decodeURIComponent !== 'undefined'
@@ -49,6 +45,7 @@ console.log = function (msg) {
             elem.appendChild(txt);
         }
     }
+
     process.stdout.write(msg + '\n');
 
     if (typeof originalLog === 'function') {
@@ -56,3 +53,59 @@ console.log = function (msg) {
     }
     else if (originalLog) return originalLog(arguments[0]);
 };
+
+var parse_stream = parser(function(results) {
+    var failed = results.fail;
+    window.zuul_results = {
+        failed: failed,
+        passed: failed.length === 0
+    };
+});
+
+parse_stream.on('assert', function(res) {
+   var ok = res.ok ? 'ok' : 'not ok';
+
+   console.log('res', res);
+   console.log(ok);
+});
+
+parse_stream.on('extra', function(res) {
+});
+
+process.stdout.pipe(parse_stream);
+
+load_script('/__zuul/test-bundle.js', run);
+
+function run(err) {
+  if (err) {
+    window.zuul_results = {
+      failures: 0,
+      passed: false
+    };
+    return;
+  }
+
+  var map = undefined;
+  var mapper = undefined;
+
+  var opt = {
+    path: '/__zuul/test-bundle.map.json'
+  };
+
+  http.get(opt, function(res) {
+    var body = '';
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+
+    res.on('end', function() {
+      map = JSON.parse(body);
+      mapper = stack_mapper(map);
+
+      do_run();
+    });
+  });
+
+  function do_run() {
+  };
+}
